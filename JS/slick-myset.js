72a6083cee8d6ab379016e42dc98e79a31aa3844
Miley Ho <miley.ho@hyweb.com.tw>
2019-10-17 16:01:27 +0800

    第一次

---
+++ JS/slick-myset.js
@@ -0,0 +1,53 @@
+$(function(){
+    // 單張輪播
+    $('.singleplay').slick({
+        arrows: true,
+        slidesToShow: 1,         //一次顯示幾張
+        slidesToScroll: 1,       //一次卷幾張
+        dots: true,              //顯示圓點
+        dotsClass: 'slick-dots', //圓點樣式
+        // 自訂左右箭頭
+        // next arrow
+        prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+        // next arrow
+        nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+        infinite: true,          //無限輪播
+        autoplay: true,          //自動輪播
+        autoplaySpeed: 3000      //輪播速度
+    });
+
+    // 多張輪播 一次播5張
+    $('.autoplay-set').slick({
+        arrows: true,
+        slidesToShow: 5,         //一次顯示幾張
+        slidesToScroll: 5,       //一次卷幾張
+        dots: true,              //顯示圓點
+        dotsClass: 'slick-dots', //圓點樣式
+        // 自訂左右箭頭
+        // next arrow
+        prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+        // next arrow
+        nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+        infinite: true,          //無限輪播
+        autoplay: true,          //自動輪播
+        autoplaySpeed: 3000      //輪播速度
+    });
+
+    // 多張輪播
+    $('.autoplay').slick({
+        arrows: true,
+        slidesToShow: 5,         //一次顯示幾張
+        slidesToScroll: 1,       //一次卷幾張
+        dots: true,              //顯示圓點
+        dotsClass: 'slick-dots', //圓點樣式
+        // 自訂左右箭頭
+        // next arrow
+        prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+        // next arrow
+        nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+        infinite: true,          //無限輪播
+        autoplay: true,          //自動輪播
+        autoplaySpeed: 3000      //輪播速度
+    });
+
+});
